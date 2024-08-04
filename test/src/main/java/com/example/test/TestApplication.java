42fe6cb05be941c495eb2a154723d0d6501a24ad
TaeWon <ytw9699@naver.com>
2024-08-05 00:14:00 +0900

    test3

---
+++ test/src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+    //test0
+    //test
+    //test2
+    //test3
+    //test4
+    //test5
+    //test6
+    //test7
+    //test8
+    //test9
+    //test10
+}
