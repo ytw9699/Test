5c8e7c8b23be0deca4d68ea9c609e88a5a68c3bb
taewon <>
2025-01-09 11:40:47 +0900

    chore: 로그 찍기

--- test/src/main/java/com/example/test/controller/TestController.java
+++ test/src/main/java/com/example/test/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.test.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/data")
+@Slf4j
 public class TestController {
 
     @GetMapping("/test")
     public String test(){
+        log.info("test");
         return  "hello";
     }
 }
